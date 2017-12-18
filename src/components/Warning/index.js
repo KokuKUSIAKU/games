e73909424300300d076b001c779cbd79643910f3
KokuKUSIAKU <kusiaku.k@gmail.com>
2017-12-18 14:37:36 +0000

    add warning message

---
+++ src/components/Warning/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./style.scss";
+
+const Warning = () => (
+  <section id="waring">
+    This project is developed using Chrome and not it is fully tested in other browsers yet <br></br>
+    A dysfunction has been observed with Firefox for instance. The game will be available for major browser soon!
+  </section>
+);
+
+export default Warning; 
--- src/components/index.js
+++ src/components/index.js
@@ -4,11 +4,13 @@ import Welcome from "./Welcome";
 import PartyLayout from "./PartyLayout";
 import PageHeader from "./PageHeader"; 
 import Rules from "./Rules";
+import Warning from "./Warning";
 
 export {
   GameOptions, 
   Welcome, 
   PartyLayout, 
   PageHeader, 
-  Rules
+  Rules, 
+  Warning
 }; 
