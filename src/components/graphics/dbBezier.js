75db948d919bb40be83f47c3db2927041117ac38
KokuKUSIAKU <kusiaku.k@gmail.com>
2018-01-05 12:21:51 +0000

    style update

--- src/components/graphics/dbBezier.js
+++ src/components/graphics/dbBezier.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const DBBezier = () => (
   <svg className="graphic-dbbezier" width="50" height="50">
-    <path d="M10 25Q25 -25 40 25 25 75 10 25" stroke="darkblue"
-      fill="darkblue" transform="rotate(-45) translate(-25,10)" />
+    <path d="M10 25Q25 -25 40 25 25 75 10 25" stroke="#C7EFCF"
+      fill="#C7EFCF" transform="rotate(-45) translate(-25,10)" />
   </svg>
 );
 
--- src/components/graphics/sqBezier.js
+++ src/components/graphics/sqBezier.js
@@ -3,7 +3,7 @@ import React from "react";
 const SQBezier = () => (
   <svg className="graphi-sqbezier" width="50" height="50">
     <path d="M5 5Q25 10 45 5 40 25 45 45 25 40 5 45 10 25 5 5"
-      fill="green" stroke="green" />
+      fill="#FE5F55" stroke="#FE5F55" />
   </svg>
 );
 
